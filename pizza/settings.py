0384c8226814eb158464d7735bf88a489a45c44b
Monique <meyer.monique93@gmail.com>
2020-06-27 17:43:47 +0200

    Whitenoise

--- pizza/settings.py
+++ pizza/settings.py
@@ -27,7 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['pinocchiopizza.herokuapp.com', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,5 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware')
 
 django_heroku.settings(locals())
