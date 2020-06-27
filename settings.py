7ca4c9837360e1a93b62442a1b411736eed9904f
Monique <meyer.monique93@gmail.com>
2020-06-27 17:06:29 +0200

    Updating settings

--- settings.py
+++ settings.py
@@ -2,4 +2,16 @@
 import django_heroku
 import os
 
-django_heroku.settings(locals(), staticfiles="orders/static")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+django_heroku.settings(locals())
